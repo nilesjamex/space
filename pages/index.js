e95d7f661e52d5828be4a8c14c6964fe57e3f19b
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-01-20 13:24:27 +0100

    cleaned up pages

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,15 @@ import styles from '../styles/Home.module.css';
 import Nav from "../Layout/Nav";
 import Homee from './../Components/Home.js';
 
+
 export default function Home() {
+   {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Space App" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-      </Head>
-
-      <main className={styles.main}>
-       <Homee />
-      </main>
+     <main className={styles.main}>
+      <Homee />
+       </main>
     </div>
   )
+}
 }
